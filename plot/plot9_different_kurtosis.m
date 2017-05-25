91579b0eefe4ee62a07868da8d45dbfada504b47
wurahara <wurahara@126.com>
2017-05-25 22:36:50 +0800

    312312

--- plot/plot9_different_kurtosis.m
+++ plot/plot9_different_kurtosis.m
@@ -6,13 +6,13 @@ y4 =gaussmf(x,[9 0]);
 
 
 
-plot(x,y1,'k-','linewidth', 1)
+plot(x,y1,'k-','linewidth', 1.5)
 hold on
-plot(x, y2,'k--','linewidth', 1);
+plot(x, y2,'k--','linewidth', 1.5);
 hold on
-plot(x, y3,'k-.','linewidth', 1);
+plot(x, y3,'k-.','linewidth', 1.5);
 hold on
-plot(x, y4,'k:','linewidth', 1);
+plot(x, y4,'k:','linewidth', 1.5);
 legend('3.90','2.08','1.59','1.54');
 k1 = kurtosis(y1,0);
 k2 = kurtosis(y2,0);
