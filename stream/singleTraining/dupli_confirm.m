df61ae1c1106b4777e69c9c38dc1fc194b5fa1f2
wurahara <wurahara@126.com>
2017-08-29 09:59:52 +0800

    rearrange file structure

--- stream/singleTraining/dupli_confirm.m
+++
@@ -1,6 +0,0 @@
-line = size(mat_error, 1);
-for i = 2 : line
-    if(mat_error(i-1, 3) == mat_error(i, 3));
-        mat_error(i, 3) = mat_error(i, 3) + 0.000000001;
-    end
-end
--- stream/singleTraining/time_format_change_test.m
+++
@@ -1,9 +0,0 @@
-time_number_1 = time_manip_1;
-
-time_string = [];
-
-line_time = size(time_number_1,2);
-
-for i = 1 : line_time
-    time_string(1, i) = datestr(time_number_1(1, i), 'MM:SS.FFF');
-end
