243d86d32a3a7546f9b646d1d3273dad088c2ff8
wurahara <wurahara@126.com>
2017-09-05 21:35:43 +0800

    modefied MFCC(unfinished)

--- test/MFCC_script.m
+++ test/MFCC_script.m
@@ -1,42 +1,48 @@
-%% filter bank definition
+clear;clc;
+%% filter bank basic settings
 load('mat_interped_test.mat');
 sig = mat_test;
 length = size(sig, 1);
 
-total_time = mat(Length, 1) - mat(1, 1);
+total_time = sig(length, 1) - sig(1, 1);
 total_time = total_time * 24 * 60 * 60;
 
 fs = length / total_time;                   % Sampling Frequency
 Period = 1 / fs;                            % Sampling Period
 
-Time_vector = mat(:,1);                     % Time vector
+Time_vector = sig(:,1);                     % Time vector
 acc_x = sig(:,3);
 
-bank=melbankm(24,256,fs,0,0.4,'t'); %Mel滤波器的阶数为24，FFT变换的长度为256，采样频率为16000Hz  
-%归一化Mel滤波器组系数  
-bank=full(bank); %full() convert sparse matrix to full matrix  
-bank=bank/max(bank(:));  
-for k=1:12  
+bank = filter_bank_mel(20, 1000, fs, 0, 0.5);
+
+bank = full(bank);                          % full() convert sparse matrix to full matrix  
+bank = bank / max(bank(:));                 % bank normalize
+
+%% DCT Coefficient
+for k=1:12
     n=0:23;  
-    dctcoef(k,:)=cos((2*n+1)*k*pi/(2*24));  
-end  
-w=1+6*sin(pi*(1:12)./12);%归一化倒谱提升窗口  
-w=w/max(w);%预加重滤波器  
+    DCT_coef(k, :)=cos( (2*n + 1) * k * pi / (2*24) );
+end
+
+w = 1 + 6 * sin( pi*(1 : 12) ./ 12);        %归一化倒谱提升窗口  
+w = w / max(w);                             %预加重滤波器  
 
 %%
-xx=double(x);  
-xx=filter(1-0.9375,1,xx);%语音信号分帧  
-xx=enframe(xx,256,80);%对xx 256点分为一帧  
-%计算每帧的MFCC参数  
-for i=1:size(xx,1)  
-    y=xx(i,:);  
-    s=y'.*hamming(256);  
-    t=abs(fft(s));%FFT快速傅里叶变换  
-    t=t.^2;  
-    c1=dctcoef*log(bank*t(1:129));  
-    c2=c1.*w';  
-    m(i,:)=c2;  
-end
+% %%
+% xx=double(x);  
+% xx=filter(1-0.9375,1,xx);%语音信号分帧  
+% xx=enframe(xx,256,80);%对xx 256点分为一帧  
+% %计算每帧的MFCC参数  
+% for i=1:size(xx,1)  
+%     y=xx(i,:);  
+%     s=y'.*hamming(256);  
+%     t=abs(fft(s));%FFT快速傅里叶变换  
+%     t=t.^2;  
+%     c1=dctcoef*log(bank*t(1:129));  
+%     c2=c1.*w';  
+%     m(i,:)=c2;  
+% end
+
 
 %%
 % %% 求一阶差分系数
