738f34f4e32f307e071a91f52c52f1752fd322f5
wurahara <wurahara@126.com>
2017-12-05 22:28:09 +0800

    spare some

---
+++ test/spare_target.m
@@ -0,0 +1,40 @@
+function [eye_target] = spare_target(target)
+
+length = size(target, 1);
+eye_target = [];
+
+for i = 1 : length
+    if(target(i, 1) == 1)
+        eye_target = [eye_target;1,0,0,0,0,0,0,0,0,0];
+    end
+    if(target(i, 1) == 2)
+        eye_target = [eye_target;0,1,0,0,0,0,0,0,0,0];
+    end
+    if(target(i, 1) == 3)
+        eye_target = [eye_target;0,0,1,0,0,0,0,0,0,0];
+    end
+    if(target(i, 1) == 4)
+        eye_target = [eye_target;0,0,0,1,0,0,0,0,0,0];
+    end
+    if(target(i, 1) == 5)
+        eye_target = [eye_target;0,0,0,0,1,0,0,0,0,0];
+    end
+    if(target(i, 1) == 6)
+        eye_target = [eye_target;0,0,0,0,0,1,0,0,0,0];
+    end
+    if(target(i, 1) == 7)
+        eye_target = [eye_target;0,0,0,0,0,0,1,0,0,0];
+    end
+    if(target(i, 1) == 8)
+        eye_target = [eye_target;0,0,0,0,0,0,0,1,0,0];
+    end
+    if(target(i, 1) == 9)
+        eye_target = [eye_target;0,0,0,0,0,0,0,0,1,0];
+    end
+    if(target(i, 1) == 0)
+        eye_target = [eye_target;0,0,0,0,0,0,0,0,0,1];
+    end
+end
+
+end
+
